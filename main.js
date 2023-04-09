e46ad1aae6b02c15ac7d84ba53cb22d6d76b2e75
Hodjaev Azamat <66169608+H-Azamat@users.noreply.github.com>
2023-04-10 01:57:15 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,664 @@
+const answers = [
+  {
+    answer: "МАТРОСОМ",
+    cells: [
+      {
+        x: 2,
+        y: 2
+      },
+      {
+        x: 2,
+        y: 3
+      },
+      {
+        x: 2,
+        y: 4
+      },
+      {
+        x: 2,
+        y: 5
+      },
+      {
+        x: 2,
+        y: 6
+      },
+      {
+        x: 2,
+        y: 7
+      },
+      {
+        x: 2,
+        y: 8
+      },
+      {
+        x: 2,
+        y: 9
+      }
+    ]
+  },
+  {
+    answer: "РЕЖИССЁР",
+    cells: [
+      {
+        x: 2,
+        y: 5
+      },
+      {
+        x: 3,
+        y: 5
+      },
+      {
+        x: 4,
+        y: 5
+      },
+      {
+        x: 5,
+        y: 5
+      },
+      {
+        x: 6,
+        y: 5
+      },
+      {
+        x: 7,
+        y: 5
+      },
+      {
+        x: 8,
+        y: 5
+      },
+      {
+        x: 9,
+        y: 5
+      }
+    ]
+  },
+  {
+    answer: "ДИРЕКТОРОМ",
+    cells: [
+      {
+        x: 4,
+        y: 8
+      },
+      {
+        x: 5,
+        y: 8
+      },
+      {
+        x: 6,
+        y: 8
+      },
+      {
+        x: 7,
+        y: 8
+      },
+      {
+        x: 8,
+        y: 8
+      },
+      {
+        x: 9,
+        y: 8
+      },
+      {
+        x: 10,
+        y: 8
+      },
+      {
+        x: 11,
+        y: 8
+      },
+      {
+        x: 12,
+        y: 8
+      },
+      {
+        x: 13,
+        y: 8
+      }
+    ]
+  },
+  {
+    answer: "СРОСТКИ",
+    cells: [
+      {
+        x: 9,
+        y: 4
+      },
+      {
+        x: 9,
+        y: 5
+      },
+      {
+        x: 9,
+        y: 6
+      },
+      {
+        x: 9,
+        y: 7
+      },
+      {
+        x: 9,
+        y: 8
+      },
+      {
+        x: 9,
+        y: 9
+      },
+      {
+        x: 9,
+        y: 10
+      }
+    ]
+  },
+  {
+    answer: "СТАЛИН",
+    cells: [
+      {
+        x: 2,
+        y: 11
+      },
+      {
+        x: 3,
+        y: 11
+      },
+      {
+        x: 4,
+        y: 11
+      },
+      {
+        x: 5,
+        y: 11
+      },
+      {
+        x: 6,
+        y: 11
+      },
+      {
+        x: 7,
+        y: 11
+      }
+    ]
+  },
+  {
+    answer: "БОНДАРЧУК",
+    cells: [
+      {
+        x: 11,
+        y: 3
+      },
+      {
+        x: 11,
+        y: 4
+      },
+      {
+        x: 11,
+        y: 5
+      },
+      {
+        x: 11,
+        y: 6
+      },
+      {
+        x: 11,
+        y: 7
+      },
+      {
+        x: 11,
+        y: 8
+      },
+      {
+        x: 11,
+        y: 9
+      },
+      {
+        x: 11,
+        y: 10
+      },
+      {
+        x: 11,
+        y: 11
+      }
+    ]
+  },
+  {
+    answer: "ЛЕНИНСКУЮ",
+    cells: [
+      {
+        x: 7,
+        y: 7
+      },
+      {
+        x: 7,
+        y: 8
+      },
+      {
+        x: 7,
+        y: 9
+      },
+      {
+        x: 7,
+        y: 10
+      },
+      {
+        x: 7,
+        y: 11
+      },
+      {
+        x: 7,
+        y: 12
+      },
+      {
+        x: 7,
+        y: 13
+      },
+      {
+        x: 7,
+        y: 14
+      },
+      {
+        x: 7,
+        y: 15
+      },
+      {
+        x: 7,
+        y: 16
+      }
+    ]
+  },
+  {
+    answer: "СЛЕСАРЕМ",
+    cells: [
+      {
+        x: 13,
+        y: 1
+      },
+      {
+        x: 13,
+        y: 2
+      },
+      {
+        x: 13,
+        y: 3
+      },
+      {
+        x: 13,
+        y: 4
+      },
+      {
+        x: 13,
+        y: 5
+      },
+      {
+        x: 13,
+        y: 6
+      },
+      {
+        x: 13,
+        y: 7
+      },
+      {
+        x: 13,
+        y: 8
+      }
+    ]
+  },
+  {
+    answer: "ЧЕТЫРЕ",
+    cells: [
+      {
+        x: 0,
+        y: 14
+      },
+      {
+        x: 1,
+        y: 14
+      },
+      {
+        x: 2,
+        y: 14
+      },
+      {
+        x: 3,
+        y: 14
+      },
+      {
+        x: 4,
+        y: 14
+      },
+      {
+        x: 5,
+        y: 14
+      }
+    ]
+  },
+  {
+    answer: "ФЛОТЕ",
+    cells: [
+      {
+        x: 5,
+        y: 10
+      },
+      {
+        x: 5,
+        y: 11
+      },
+      {
+        x: 5,
+        y: 12
+      },
+      {
+        x: 5,
+        y: 13
+      },
+      {
+        x: 5,
+        y: 14
+      }
+    ]
+  },
+  {
+    answer: "ЖЕЛУДКА",
+    cells: [
+      {
+        x: 18,
+        y: 0
+      },
+      {
+        x: 18,
+        y: 1
+      },
+      {
+        x: 18,
+        y: 2
+      },
+      {
+        x: 18,
+        y: 3
+      },
+      {
+        x: 18,
+        y: 4
+      },
+      {
+        x: 18,
+        y: 5
+      },
+      {
+        x: 18,
+        y: 6
+      }
+    ]
+  },
+  {
+    answer: "РАССКАЗ",
+    cells: [
+      {
+        x: 13,
+        y: 6
+      },
+      {
+        x: 14,
+        y: 6
+      },
+      {
+        x: 15,
+        y: 6
+      },
+      {
+        x: 16,
+        y: 6
+      },
+      {
+        x: 17,
+        y: 6
+      },
+      {
+        x: 18,
+        y: 6
+      },
+      {
+        x: 19,
+        y: 6
+      }
+    ]
+  },
+  {
+    answer: "МОСКВУ",
+    cells: [
+      {
+        x: 15,
+        y: 4
+      },
+      {
+        x: 15,
+        y: 5
+      },
+      {
+        x: 15,
+        y: 6
+      },
+      {
+        x: 15,
+        y: 7
+      },
+      {
+        x: 15,
+        y: 8
+      },
+      {
+        x: 15,
+        y: 9
+      }
+    ]
+  },
+  {
+    answer: "КАЛИНЫ",
+    cells: [
+      {
+        x: 7,
+        y: 13
+      },
+      {
+        x: 8,
+        y: 13
+      },
+      {
+        x: 9,
+        y: 13
+      },
+      {
+        x: 10,
+        y: 13
+      },
+      {
+        x: 11,
+        y: 13
+      },
+      {
+        x: 12,
+        y: 13
+      }
+    ]
+  },
+  {
+    answer: "КЛАДОВЩИК",
+    cells: [
+      {
+        x: 11,
+        y: 11
+      },
+      {
+        x: 12,
+        y: 11
+      },
+      {
+        x: 13,
+        y: 11
+      },
+      {
+        x: 14,
+        y: 11
+      },
+      {
+        x: 15,
+        y: 11
+      },
+      {
+        x: 16,
+        y: 11
+      },
+      {
+        x: 17,
+        y: 11
+      },
+      {
+        x: 18,
+        y: 11
+      },
+      {
+        x: 19,
+        y: 11
+      }
+    ]
+  },
+  {
+    answer: "ВГИК",
+    cells: [
+      {
+        x: 15,
+        y: 8
+      },
+      {
+        x: 16,
+        y: 8
+      },
+      {
+        x: 17,
+        y: 8
+      },
+      {
+        x: 18,
+        y: 8
+      }
+    ]
+  },
+  {
+    answer: "ВЫСОЦКИЙ",
+    cells: [
+      {
+        x: 16,
+        y: 11
+      },
+      {
+        x: 16,
+        y: 12
+      },
+      {
+        x: 16,
+        y: 13
+      },
+      {
+        x: 16,
+        y: 14
+      },
+      {
+        x: 16,
+        y: 15
+      },
+      {
+        x: 16,
+        y: 16
+      },
+      {
+        x: 16,
+        y: 17
+      },
+      {
+        x: 16,
+        y: 18
+      }
+    ]
+  },
+  {
+    answer: "ИНФАРКТА",
+    cells: [
+      {
+        x: 11,
+        y: 16
+      },
+      {
+        x: 12,
+        y: 16
+      },
+      {
+        x: 13,
+        y: 16
+      },
+      {
+        x: 14,
+        y: 16
+      },
+      {
+        x: 15,
+        y: 16
+      },
+      {
+        x: 16,
+        y: 16
+      },
+      {
+        x: 17,
+        y: 16
+      },
+      {
+        x: 18,
+        y: 16
+      }
+    ]
+  }
+];
+
+const items = document.querySelectorAll(".answers-item");
+
+const showAnswer = (answerData, target, item) => {
+  for (let i = 0; i < answerData.answer.length; i++) {
+    const cell = document.querySelector(
+      `[data-id='${answerData.cells[i].y} ${answerData.cells[i].x}']`
+    );
+
+    cell.innerText = answerData.answer[i];
+
+    target.closest(".answers-actions").classList.add("hidden");
+
+    item.querySelector(
+      ".answers-result"
+    ).innerText = answerData.answer.toLowerCase();
+  }
+};
+
+items.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const id = Number(item.getAttribute("data-id"));
+    const target = e.target;
+    const answerData = answers[id - 1];
+
+    if (target.classList.contains("enter")) {
+      const answer = prompt("Введите ответ для " + id + " вопроса:")
+        .toUpperCase()
+        .trim();
+
+      if (answer === answerData.answer) {
+        showAnswer(answerData, target, item);
+      } else if (answer === "LUCHI" || answer === "ЛУЧИ") {
+        alert("Да, она самая лучшая! <3");
+      } else if (id === 9 && answer === "ЧОТЫРЕ") {
+        alert("Блаженство наступает лишь перед самым выстрелом.");
+      } else {
+        alert("Ответ неверный :(");
+      }
+    } else if (target.classList.contains("show-answer")) {
+      showAnswer(answerData, target, item);
+    }
+  });
+});
